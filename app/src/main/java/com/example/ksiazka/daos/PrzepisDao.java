8d1bf26011e49af7740d154c719381a827b99be6
AgnieszkaBod <agnieszka.b@protonmail.com>
2021-12-28 20:04:43 +0100

    improve the update

--- app/src/main/java/com/example/ksiazka/daos/PrzepisDao.java
+++ app/src/main/java/com/example/ksiazka/daos/PrzepisDao.java
@@ -20,10 +20,10 @@ public interface PrzepisDao {
     @Query("SELECT name FROM przepis")
     List<String> showAll();
 
-//    @Query("UPDATE przepis SET name=(:name), wykonanie=(:wykonanie), skladnik0=(:skladnik0)," +
-//            "skladnik1=(:skladnik1), skladnik2=(:skladnik2),skladnik3=(:skladnik3), skladnik4=(:skladnik4), " +
-//            "skladnik5=(:skladnik5), skladnik6=(:skladnik6) WHERE id=(:id)")
-//    PrzepisEntity update(Integer id, String name, String wykonanie, String skladnik0, String skladnik1,
-//                         String skladnik2, String skladnik3, String skladnik4, String skladnik5,
-//                         String skladnik6); -- sprawdzić
+    @Query("UPDATE przepis SET name=(:name), wykonanie=(:wykonanie), skladnik0=(:skladnik0)," +
+            "skladnik1=(:skladnik1), skladnik2=(:skladnik2),skladnik3=(:skladnik3), skladnik4=(:skladnik4), " +
+            "skladnik5=(:skladnik5), skladnik6=(:skladnik6) WHERE id =:id")
+  void update(Long id, String name, String wykonanie, String skladnik0, String skladnik1,
+                         String skladnik2, String skladnik3, String skladnik4, String skladnik5,
+                         String skladnik6);
 }
